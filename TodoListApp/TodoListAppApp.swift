2ecb6e3c3f5d5bf4b6d59fc1392b7f91e4af6a7c
PhilipHanlon <phil.hanlon34@gmail.com>
2024-04-10 08:46:17 +0100

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by philip hanlon on 10/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
